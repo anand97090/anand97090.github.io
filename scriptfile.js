26702d112543c237ee6de184248cab71d8fc52b7
anand97090 <62260044+anand97090@users.noreply.github.com>
2020-03-19 00:38:09 +0530

    MyWebsite
    Made By me.

---
+++ scriptfile.js
@@ -0,0 +1,26 @@
+function workexp()
+{
+   
+   window.open('file:///C:/MyWeb/workexp.html', '_blank');
+}
+function education()
+{
+   
+   window.open('file:///C:/MyWeb/education.html', '_blank');
+   
+}
+function contact()
+{
+   
+   window.open('file:///C:/MyWeb/contact.html', '_blank');
+}
+function skill()
+{
+   
+   window.open('file:///C:/MyWeb/skill.html', '_blank');
+}
+function certificate()
+{
+   
+   window.open('file:///C:/MyWeb/certificate.html', '_blank');
+}
